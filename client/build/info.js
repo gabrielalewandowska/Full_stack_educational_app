f4729c79627cd9ce5693a9300a8f8643266f1863
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-11-09 16:17:12 +0000

    Remove window onload on jungle info

--- client/build/info.js
+++ client/build/info.js
@@ -187,10 +187,10 @@ for (animal of animalData) {
       var p = document.createElement('p')
       backDiv.appendChild(p)
 }
-window.addEventListener('DOMContentLoaded', function() {
-  console.log("jungle / info");
-  makeRequest(document.location.origin + "/api/animals/jungle" || 'http://localhost:3000/api/animals/jungle', renderJungleInfo)
-});
+// window.addEventListener('DOMContentLoaded', function() {
+//   console.log("jungle / info");
+//   makeRequest(document.location.origin + "/api/animals/jungle" || 'http://localhost:3000/api/animals/jungle', renderJungleInfo)
+// });
 
 
 
